fb5ce904ae6f78e7ef71257a418a47c31c436ac2
MatheusKpv <matheusmatias1910@gmail.com>
2025-06-19 11:15:46 -0300

    adicionando configurações iniciais do compose.yaml e adicionando .env

--- src/main/java/com/one/challengeforumhub/ChallengeForumHubApplication.java
+++ src/main/java/com/one/challengeforumhub/ChallengeForumHubApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ChallengeForumHubApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ChallengeForumHubApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ChallengeForumHubApplication.class, args);
+    }
 
 }
--- src/test/java/com/one/challengeforumhub/ChallengeForumHubApplicationTests.java
+++ src/test/java/com/one/challengeforumhub/ChallengeForumHubApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ChallengeForumHubApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
